12c826f2a385271e6ec5abcb0e3f774a1a365719
goyoaga <arielgoyoaga@gmail.com>
2026-05-10 19:58:37 +0200

    ci: production ready and base path fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // Si tu repositorio en GitHub se llama 'pdf-editor-app', descomenta la siguiente línea:
-  // base: "/pdf-editor-app/",
+  base: "/pdf-editor-app/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
